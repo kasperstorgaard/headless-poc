d23bf746f7a09a60881ad87effb411d2e4e4e033
kstorgaard <kst@kayak.com>
2018-12-01 13:39:59 +0100

    adds type update script

---
+++ server/update-types.js
@@ -0,0 +1,21 @@
+const {exec} = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const projectFile = fs.readFileSync(path.join(__dirname, 'secret/project.json'), 'utf8');
+const projectId = JSON.parse(projectFile).key;
+
+const args = {
+  codeType: 'TypeScript',
+  moduleResolution: 'ES2015',
+  projectId
+};
+
+const argStr = Object.keys(args).reduce((str, key) => str + ` --${key}=${args[key]}`, '');
+
+exec([
+  'cd src/types', 
+  `node ../../node_modules/.bin/kc-generate ${argStr}`
+].join(' && '), () => {
+  console.log('done!');
+});
