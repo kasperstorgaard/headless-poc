f6206f75484d5ad7236aed93683a3868516e32c6
kstorgaard <kst@kayak.com>
2018-12-01 20:51:42 +0100

    setup typescript and basic express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('starting');
+import express from 'express';
+import {join} from 'path';
+
+import {router} from './router';
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', join(__dirname, 'views'));
+app.use(router);
+app.set
+
+app.listen(3000, () => {
+  console.log('server started on localhost:3000');
+});
