659d5a01d4dc91f8a6e1f86d86605d1a41b9df2b
kstorgaard <kst@kayak.com>
2018-12-02 14:08:40 +0100

    restructure frontpage to use section items

--- server/src/cms-client.ts
+++ server/src/cms-client.ts
@@ -5,10 +5,12 @@ import {
 
 import project from '../secret/project.json';
 import { Frontpage } from './types/frontpage';
+import { HeroSection } from './types/hero_section.js';
 
 export const client = new DeliveryClient({
   projectId: project.key,
   typeResolvers: [
-      new TypeResolver('frontpage', () => new Frontpage())
+      new TypeResolver('frontpage', () => new Frontpage()),
+      new TypeResolver('hero_section', () => new HeroSection())
   ]
 });
