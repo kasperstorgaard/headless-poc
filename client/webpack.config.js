a9ea2f7a54dd4d92621237f8fe2f513cd0052ca2
kstorgaard <kst@kayak.com>
2018-12-11 14:50:01 +0100

    move service worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,11 +25,9 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
-    new WorkboxPlugin.GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast 
-      // and not allow any straggling "old" SWs to hang around
-      clientsClaim: true,
-      skipWaiting: true
+    new WorkboxPlugin.InjectManifest({
+      swSrc: './src/service-worker.js',
+      swDest: 'service-worker.js'
     })
   ]
 }
