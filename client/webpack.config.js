1eaee3fa276828a1f2cade5713e484af0c32bb88
kstorgaard <kst@kayak.com>
2018-12-11 14:43:45 +0100

    add service worker through webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -22,5 +23,13 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/static/'
-  }
+  },
+  plugins: [
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast 
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true
+    })
+  ]
 }
