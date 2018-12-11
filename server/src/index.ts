3ddffcaa20a32f562111b30e41ef540934748845
kstorgaard <kst@kayak.com>
2018-12-11 14:43:35 +0100

    switch to spdy for http2

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,42 @@
 import express from 'express';
+import spdy from 'spdy';
 import {join} from 'path';
+import {readFileSync} from 'fs';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import chalk from 'chalk';
+import logger from 'morgan';
 
-import { updateNavigation } from './content/navigation';
-import { routes } from './route-api';
+import {updateNavigation} from './content/navigation';
+import {routes} from './route-api';
 
+const port = 3000;
 const app = express();
+
+const options = {
+  key: readFileSync(join(__dirname, 'secret/server.key')),
+  cert: readFileSync(join(__dirname, 'secret/server.crt'))
+}
 
 app.set('view engine', 'pug');
 app.set('views', join(__dirname, 'views'));
-app.use('/static', express.static(join(__dirname, '../dist/static'), {}));
+
+app.use(logger('dev'))
+app.use(compression());
+app.use(cookieParser());
+app.use('/static', express.static(join(__dirname, 'static'), {}));
 app.use('/api/v1/route', routes);
 
 app.get('**', async (_req, res) => res.render('index'));
 
-app.listen(3000, () => {
-  console.log('server started on localhost:3000')
-  updateNavigation('default');
-});
+spdy
+  .createServer(options, app)
+  .listen(port, () => {
+    updateNavigation('default');
+
+    console.log(chalk.magenta([
+      '┌-----------------------------┐',
+      `| App listening on port: ${port} |`,
+      '└-----------------------------┘'
+    ].join('\n')));
+  });
