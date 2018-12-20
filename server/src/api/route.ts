4fe4d64c6c54edaaa1741efca7a0f8de46dc7c5d
kasperstorgaard <kasper.storgaard@gmail.com>
2018-12-20 12:08:09 +0100

    change routes api to not use taxonomy

--- server/src/api/route.ts
+++ server/src/api/route.ts
@@ -1,38 +1,68 @@
 import {Router} from 'express';
 import {client} from '../content/cms-client';
-import {HomeItem, PageItem} from '../content/resolvers';
+import {RouteItem} from '../content/resolvers';
 
 export const routes = Router();
 
 routes.get('**', async (req, res) => {
   const parts = req.path.replace(/(^\/|\/$)/, '').split('/');
 
-  const type = parts.length > 1 ? 'subpage' : 'page';
-  const item = await getItem(type, parts[parts.length - 1]);
+  const item = await getItem(parts);
+  const model = item ? item.toModel() : null;
 
   if (!item) {
     res.status(404).send('Route not found');
   } else {
-    res.json(item);
+    res.json(model);
   }
 })
 
-async function getItem(navigationType: 'page' | 'subpage', url: string) {
-  if (!url || url === 'home') {
+async function getItem(parts: string[]) {
+  const lastPart = parts[parts.length - 1];
+
+  if (!lastPart || lastPart === 'home') {
     const response = await client
-      .item<HomeItem>('home')
+      .item<RouteItem>('root_route')
       .depthParameter(3)
       .getPromise();
 
-    return response.isEmpty ? null : response.item.toModel();
+    return response.isEmpty ? null : response.item.page[0];
   }
 
   const response = await client
-    .items<PageItem>()
-    .containsFilter('elements.navigation', [navigationType])
+    .items<RouteItem>()
+    .equalsFilter('elements.url', lastPart)
+    .depthParameter(3)
+    .getPromise();
+
+    if (response.isEmpty) {
+      return null;
+    }
+
+    if (response.items.length > 1 && parts.length > 1) {
+      const candidates = await Promise.all(response.items.map(item =>
+        getParentMatch(item, parts[parts.length - 2])
+      ));
+
+      return candidates.find(candidate => candidate != null);
+    }
+
+    return response.items[0];
+}
+
+async function getParentMatch(child: RouteItem, url: string): Promise<RouteItem | null> {
+  const response = await client
+    .items<RouteItem>()
     .equalsFilter('elements.url', url)
     .depthParameter(3)
     .getPromise();
 
-    return response.isEmpty ? null : response.getFirstItem().toModel();
+    if (response.isEmpty) {
+      return null;
+    }
+
+    const matches = response.items.some(item =>
+      item.routes.some(route => route.system.codename === child.system.codename));
+
+    return matches ? child : null;
 }
